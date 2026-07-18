977282ff0653e92408cd37ece2d368c7a9f618a5
Adam Jenkins <adam@wisecat.net>
2026-07-18 10:56:36 +0000

    Add Japanese (ja) language pack

---
+++ lang/ja/block_oerexchangequicklinks.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_oerexchangequicklinks', language 'ja'.
+ *
+ * @package    block_oerexchangequicklinks
+ * @copyright  2026 Adam Jenkins <adam@wisecat.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['download'] = 'ダウンロード';
+$string['noquicklinks'] = 'OER Exchange で試したリソースは、すぐにアクセスできるようここに表示されます。';
+$string['oerexchangequicklinks:addinstance'] = '新しい「OER Exchange: クイックリンク」ブロックを追加する';
+$string['oerexchangequicklinks:myaddinstance'] = '新しい「OER Exchange: クイックリンク」ブロックをダッシュボードに追加する';
+$string['pluginname'] = 'OER Exchange: クイックリンク';
+$string['privacy:metadata'] = '「OER Exchange: クイックリンク」ブロックはそれ自体でデータを保存しません。local_oerexchange がすでに記録しているサンドボックス試用データを読み取り専用で表示するものであり、プライバシーの取り扱いについては local_oerexchange 側で文書化されています。';
+$string['tryit'] = '試してみる';
